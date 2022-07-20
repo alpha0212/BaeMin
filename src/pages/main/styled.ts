e7710d3e3ed56e3204483ff746ee4ed10e692b4d
HanSeo0507 <i.hansu.lee@gmail.com>
2022-07-21 04:11:21 +0900

    fix: 메인 페이지 상단 padding, 각 세션 하단 margin 수정

--- src/pages/main/styled.ts
+++ src/pages/main/styled.ts
@@ -1,10 +1,11 @@
 import styled from '@emotion/styled';
 
 export const MainPageContainer = styled.div`
+  padding-top: 2rem;
   padding-bottom: 10rem;
 
   & > section:not(:last-child) {
-    margin-bottom: 12rem;
+    margin-bottom: 8rem;
   }
 `;
 
