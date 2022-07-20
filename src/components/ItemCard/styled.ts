39268f9f00abf2913e03a78ca5b1a6fa99458fd5
HanSeo0507 <i.hansu.lee@gmail.com>
2022-07-21 03:35:32 +0900

    fix: ItemCard 컴포넌트 min-width 스타일 추가

--- src/components/ItemCard/styled.ts
+++ src/components/ItemCard/styled.ts
@@ -5,6 +5,7 @@ export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 25rem;
+  min-width: 25rem;
   height: 32rem;
   padding: 2rem;
   background: #ffffff;
