f48b5db2d0eee0a1e3b734b056f8a00f796f1248
HanSeo0507 <i.hansu.lee@gmail.com>
2022-07-20 23:20:56 +0900

    feat: Navbar 컴ㅍ포넌트 추가

---
+++ src/components/Navbar/styled.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const NavbarContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 8.4rem;
+  background: rgba(255, 255, 255, 0.8);
+  border-bottom: 1px solid #e2e3eb;
+  backdrop-filter: blur(7px);
+`;
+
+export const NavbarLogo = styled.img`
+  width: 15.9rem;
+  height: 6.2rem;
+  margin-top: 1rem;
+`;
